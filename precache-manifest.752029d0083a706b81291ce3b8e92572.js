b7ddf1de798d57fa5db690102130151526f09f8e
Pascal Nguyen <pasngu@student.42.fr>
2020-02-21 16:26:13 +0100

    Updates

---
+++ precache-manifest.752029d0083a706b81291ce3b8e92572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f9c8ade1079c733e11594907d1815c",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "b1e3beab623a3cb0b23e",
+    "url": "/monster/static/css/main.4a973153.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e3beab623a3cb0b23e",
+    "url": "/monster/static/js/main.b257142b.chunk.js"
+  },
+  {
+    "revision": "0fec111df979dce597e7",
+    "url": "/monster/static/js/runtime-main.b58bb632.js"
+  }
+]);
